f25c00143cb39c8467d46e9a7b711fc25f94f439
unknown <sajjadrezvani2000@gmail.com>
2022-03-08 17:15:26 +0330

    branch chanches

--- prac.py
+++ prac.py
@@ -4,18 +4,21 @@ class cars:
     brand = 'ford'
     base= int(1995)
     def __init__(self, at1, at2 = 'Henry' ):
-        self.owner = at2 
+        self.owner = at2
+        print('im hearrrr') 
         self.name = at1 
+        
     
     def __str__(self):
-        return f'{self.name} and {self.brand}'
+        return f'{self.name} and {self.brand} and hearrrrr'
 
     def age(self):
         return datetime.datetime.now().year-self.base
 
 firstCar =cars('non','shit!')
 firstCar.base = int(input(f'pleas enter your year of production {firstCar.owner} :'))
-print(f'The age of car is:{firstCar.age()} !!')
-
+if (2 == 1):
+    print ('and not hearrrr')
 firstCar.brand= input('\nenter your car brand:')
-print(firstCar)
+
+
